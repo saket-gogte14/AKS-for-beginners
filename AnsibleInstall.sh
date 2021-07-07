a7a746540f21fbe4b59a4ed9e77ce8c5d0d3ae7d
saket-gogte14 <68593337+saket-gogte14@users.noreply.github.com>
2021-07-07 10:03:28 +0530

    Delete AnsibleInstall.sh

--- AnsibleInstall.sh
+++
@@ -1,5 +0,0 @@
-#These commands will install Ansible on your Azure VM
-sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum update -y --disablerepo='*' --enablerepo='*microsoft*'
-sudo yum -y install ansible
-ansible --version
