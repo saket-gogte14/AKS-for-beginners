bc4410f5b3e38c3e07ecb7dc3264c32b0c80507f
saket-gogte14 <68593337+saket-gogte14@users.noreply.github.com>
2021-07-07 10:03:17 +0530

    Update AnsibleInstall.sh

--- AnsibleInstall.sh
+++ AnsibleInstall.sh
@@ -1,3 +1,4 @@
+#These commands will install Ansible on your Azure VM
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum update -y --disablerepo='*' --enablerepo='*microsoft*'
 sudo yum -y install ansible
